12fcf9529d7f7f163569657192ed13594466a8b4
Philipp Panhey <philipp.panhey@gmail.com>
2023-03-15 12:01:47 +0100

    adjusted password workflow

--- password.py
+++ password.py
@@ -2,9 +2,9 @@ import string
 import random
 import sys
 
-characters = string.ascii_letters + string.punctuation  + string.digits
+characters = string.ascii_letters + string.digits
 password = ""
-password_length = random.randint(8, 16)
+password_length = random.randint(14, 20)
 for x in range(password_length):
     char = random.choice(characters)
     password = password + char
